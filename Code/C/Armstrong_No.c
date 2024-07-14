4e1bd03a64005288b76c5a95075f2d24d3527c6a
Vamshi-27 <vamshira18@outlook.com>
2024-07-14 21:29:09 +0530

    sync

--- Code/C/Armstrong_No.c
+++ Code/C/Armstrong_No.c
@@ -6,26 +6,31 @@
 int main()
 {
     int num, originalNum, remainder, n = 0;
-    float result = 0.0;
+    float result = 0.0; // Corrected variable name from 'resul' to 'result'
 
     printf("Enter an integer:\n");
-    scanf("%d",&num);
+    scanf("%d", &num);
 
-    originalNum = num;
+    originalNum = num; // Added missing semicolon
 
+    // Calculate the number of digits in num
     for (originalNum = num; originalNum != 0; ++n)
     {
         originalNum /= 10;
     }
 
+    // Calculate the sum of the nth power of its digits
     for (originalNum = num; originalNum != 0; originalNum /= 10)
     {
         remainder = originalNum % 10;
         result += pow(remainder, n);
     }
+
+    // Check if the sum is equal to the original number
     if ((int)result == num)
         printf("%d is an Armstrong number.", num);
     else
         printf("%d is not an Armstrong number.", num);
+
     return 0;
 }
